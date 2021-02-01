1a7f1ae84bac22ad1eba1a020f778c1bf75fdf9b
Ryan Hsieh <chengyu0827@gmail.com>
2021-02-01 19:22:14 +0800

    Edit react application to make API calls

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,14 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data1, setData1] = useState('');
+  const [data2, setData2] = useState('');
+
+  useEffect(() => {
+    fetch('node/api/message')
+      .then(res => res.json())
+      .then(data => {
+        setData1(data.message);
+      });
+    fetch('flask/api/message')
+      .then(res => res.json())
+      .then(data => {
+        setData2(data.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +31,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>{data1}</p>
+        <p>{data2}</p>
       </header>
     </div>
   );
